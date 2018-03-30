e3a2f5a73b8533b869d2cba8bd2a0e1e5be9e3a1
alishaevn <alieva.inc@gmail.com>
2018-03-30 15:42:55 -0700

    created component

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import './main.css'
+
+export default class Main extends Component {
+  render() {
+
+    return (
+      <div className='main-component'>
+        <p className='first-name'>alisha</p>
+        <p className='middle-name'>antoinette</p>
+      </div>
+    )
+
+  }
+
+}
+
+
+/**
+ * Color palette - 'https://coolors.co/030d44-eaebff-134b72-f05d5e-fefffe'
+ */
