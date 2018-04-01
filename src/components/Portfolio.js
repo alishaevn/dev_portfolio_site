14e95b3e7f5b5ad2576949fb60c1ae188ccbd6fd
alishaevn <alieva.inc@gmail.com>
2018-04-01 16:42:40 -0700

    styling and layout

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
 
 import './portfolio.css'
 
@@ -7,11 +8,19 @@ export default class Portfolio extends Component {
 
     return (
       <div className='portfolio-component' id='portfolio'>
-        <div className='col-one' >
+        <h3 className='portfolio-title'>PORTFOLIO</h3>
+        <div className='row-one' >
           <div className='project-one'>
             <img src={require('../images/FTW_Mock.png')} alt='For The Win Mockup' className='ftw-mock' />
-            <h5>For The Win</h5>
-            <p>The basketball enthusiast's favorite way to track stats for their favorite players.</p>
+            <h5>
+              <a 
+                href='https://for-the-win-app.netlify.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='portfolio-project-name'
+                >For The Win</a>
+            </h5>
+            <p className='portfolio-project-description'>The basketball enthusiast's favorite way to track stats for their favorite players.</p>
           </div>
         </div>
       </div>
