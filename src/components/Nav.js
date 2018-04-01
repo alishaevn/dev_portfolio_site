8a66f0416aa094f83c036d89579357f8c0e10eea
alishaevn <alieva.inc@gmail.com>
2018-04-01 14:01:40 -0700

    more styling of the social icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
-import { SocialIcons } from 'react-social-icons';
+import { SocialIcon } from 'react-social-icons';
 
 export default class Nav extends Component {
   render() {
-    const urls=[
-      'https://www.linkedin.com/in/alishaevans/',
-      'https://github.com/alishaevn'
-    ]
+
+    const socialIconStyles = {
+      height: 30,
+      width: 30,
+      'margin-right': 8
+    }
 
     return (
       <div className='nav-component'>
         <div className='social-media-icons'>
-          <SocialIcons urls={urls} color='black' style={{ height: 5, width: 5 }} />
+          <SocialIcon url='https://www.linkedin.com/in/alishaevans/' color='black' style={socialIconStyles} />
+          <SocialIcon url='https://github.com/alishaevn' color='black' style={socialIconStyles} />
         </div>
         <div className='navigation-links'>
           <p>About Me</p>
@@ -28,4 +31,5 @@ export default class Nav extends Component {
  Resources:
   - http://jaketrent.github.io/react-social-icons/
   - <SocialIcons urls={urls} className='social-media-link' />
+  - <SocialIcons urls={urls} color='black' style={socialIconStyles} />
  */
