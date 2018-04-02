3ff91ab83b986ca00ca9b42aa620feabdba16f7a
alishaevn <alieva.inc@gmail.com>
2018-04-01 17:53:29 -0700

    added email social icon plus copyright info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,7 +17,10 @@ export default class Footer extends Component {
         <div className='social-media-icons'>
           <SocialIcon url='https://www.linkedin.com/in/alishaevans/' style={socialIconStyles} color='#E0B1CB' />
           <SocialIcon url='https://github.com/alishaevn' style={socialIconStyles} color='#E0B1CB' />
+          <SocialIcon url='mailto:alieva.inc@gmail.com' style={socialIconStyles} color='#E0B1CB' />
         </div>
+        <p className='copyright-info'>© Copyright Alisha Antoinette Evans 2018.</p>
+        <p className='copyright-info'>All Rights Reserved.</p>
       </div>
     )
 
