7a5a951a2915877481d75465a77c716ae951585d
alishaevn <alieva.inc@gmail.com>
2018-04-02 23:47:07 -0700

    added route for the forthewin component

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Main from './components/Main'
+import ForTheWin from './components/portfolio/ForTheWin'
 
 
 export default class Routes extends Component {
@@ -14,6 +15,7 @@ export default class Routes extends Component {
         <div className='appPage'>
           <main className='main' role='main'>
             <Route exact path='/' component={Main} />
+            <Route exact path='/forthewin' component={ForTheWin} />
           </main>
         </div>
       </Router>
