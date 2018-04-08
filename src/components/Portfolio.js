7444341e69dcd6cd8aeabd15e75e1484243344e8
alishaevn <alieva.inc@gmail.com>
2018-04-07 23:20:58 -0700

    finished the new layout && styling

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -33,14 +33,33 @@ export default class Portfolio extends Component {
       <div className='portfolio-component' id='portfolio'>
         <h3 className='portfolio-title'>PORTFOLIO</h3>
         <div className='row-one' >
+
           <div className='project-one'>
-            <Link
-              to='/forthewin'
-              className='project-image-link'
-            >
-              <img src={require('../images/portfolio/ftw_mock.png')} alt='For The Win Mockup' className='portfolio-homepage-mock' />
-            </Link>              
+            <img src={require('../images/portfolio/ftw_responsive.png')} alt='For The Win Mockup' className='portfolio-responsive-mock' />
+            <p className='portfolio-project-title'>For The Win</p>
+            <p className='portfolio-services'>FULL STACK DEVELOPMENT • UI/UX DESIGN</p>
+            <p className='portfolio-project-tagline'>
+              The basketball enthusiast's preferred way to track stats for their favorite players.
+            </p>
+            <p classname='live-porfolio-par'>
+              <a
+                href='https://for-the-win-app.netlify.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='live-portfolio-link'
+              >View the Live Site
+              </a>
+            </p>
+            <p classname='github-repo-par'>
+              <a
+                href='https://github.com/thinkful-ei18/for_the_win_client'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='github-repo-link'
+              >Github Repo</a>
+            </p>
           </div>
+
         </div>
       </div>
     )
