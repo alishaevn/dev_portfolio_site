3d7850e55f41956e9513588b59dee15ba8c57d8d
alishaevn <alieva.inc@gmail.com>
2018-04-20 19:20:19 -0700

    added 2nd portfolio project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,33 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 import './portfolio.css'
 
+
 export default class Portfolio extends Component {
   render() {
-
-    // const portfolio = [
-    //   {
-    //     uri: '/forthewin',
-    //     src: '../images/portfolio/ftw_mock.png',
-    //     alt: 'for the win mockup'
-    //   }
-    // ]
-
-    // const portfolioThumbnails = portfolio.forEach( project => {
-    //   console.log(
-    //   <Link
-    //     to={project.uri}
-    //     className='project-image-link'
-    //     onClick={() => handleClick()}
-    //   >
-    //     <img src={require(`${project.src}`)} alt={project.alt} className='portfolio-thumbnail' />
-    //   </Link>)
-    // })
-
-    // const handleClick = () => {
-    //   console.log('clicked')
-    // }
 
     return (
       <div className='portfolio-component' id='portfolio'>
@@ -60,19 +37,35 @@ export default class Portfolio extends Component {
             </p>
           </div>
 
+          <div className='project-two'>
+            <img src={require('../images/portfolio/fp_responsive.png')} alt='Foodie Phonetics Mockup' className='portfolio-responsive-mock' />
+            <p className='portfolio-project-title'>Foodie Phonetics</p>
+            <p className='portfolio-services'>FULL STACK DEVELOPMENT • UI/UX DESIGN</p>
+            <p className='portfolio-project-tagline'>
+              Using a spaced repetition algorithm, Foodie Phonetics teaches the user how to say the most common food items in their language of choice.
+            </p>
+            <p classname='live-porfolio-par'>
+              <a
+                href='https://foodiephonetics.netlify.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='live-portfolio-link'
+              >View the Live Site
+              </a>
+            </p>
+            <p classname='github-repo-par'>
+              <a
+                href='https://github.com/thinkful-ei18/Alisha_Brian_SpacedRep_Client'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='github-repo-link'
+              >Github Repo</a>
+            </p>
+          </div>
+
         </div>
       </div>
     )
 
   }
 }
-
-/*
-<Link
-  to='/forthewin'
-  className='project-image-link'
-  onClick={}
->
-  <img src={require('../images/portfolio/ftw_mock.png')} alt='For The Win Mockup' className='portfolio-homepage-mock' />
-</Link>
-*/
