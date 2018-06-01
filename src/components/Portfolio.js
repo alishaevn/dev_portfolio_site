ed34093b527726fbb1e18d6bf23e706cfed17eb1
alishaevn <alieva.inc@gmail.com>
2018-05-31 23:43:15 -0700

    added 3 design projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -24,57 +24,6 @@ export default class Portfolio extends Component {
               safeR 
             </a>
             <p className='portfolio-services'> FULL STACK DEVELOPMENT • UI/UX DESIGN • REACT NATIVE </p>
-            <p className='portfolio-project-tagline'>
-              Stay safer by staying up to date on local crimes & incidents in the area.
-            </p>
-            <p className='live-porfolio-par'>
-              <a
-                href='https://safer.netlify.com/'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='live-portfolio-link'
-              >View the Live Site
-              </a>
-            </p>
-            <div className='github-repos'>
-              <div>
-                <a
-                  href='https://github.com/thinkful-ei18/maajac-client'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={require('../images/github_icon.png')} alt='link to client repo' className='github-icon' />
-                </a>
-                <a
-                  href='https://github.com/thinkful-ei18/majaac-server'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={require('../images/github_icon.png')} alt='link to server repo' className='github-icon' />
-                </a>
-                <a
-                  href='https://github.com/thinkful-ei18/MOBILE-maajac-client'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={require('../images/github_icon.png')} alt='link to mobile repo' className='github-icon' />
-                </a>
-              </div>
-              <div>
-                <a href='https://github.com/thinkful-ei18/maajac-client'
-                  className='github-repo-link' > 
-                  Client 
-                </a>
-                <a href='https://github.com/thinkful-ei18/majaac-server' 
-                  className='github-repo-link'> 
-                  Server 
-                </a>
-                <a href='https://github.com/thinkful-ei18/MOBILE-maajac-client' 
-                  className='github-repo-link'> 
-                  Mobile 
-                </a>
-              </div>
-            </div>
           </div>
 
 
@@ -91,46 +40,6 @@ export default class Portfolio extends Component {
               Foodie Phonetics 
             </a>
             <p className='portfolio-services'>FULL STACK DEVELOPMENT • UI/UX DESIGN</p>
-            <p className='portfolio-project-tagline'>
-              Learn to say common food items in French using a spaced repetition algorithm.
-            </p>
-            <p className='live-porfolio-par'>
-              <a
-                href='https://foodiephonetics.netlify.com/'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='live-portfolio-link'
-              >View the Live Site
-              </a>
-            </p>
-            <div className='github-repos'>
-              <div>
-                <a
-                  href='https://github.com/thinkful-ei18/Alisha_Brian_SpacedRep_Client'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={require('../images/github_icon.png')} alt='link to client repo' className='github-icon' />
-                </a>
-                <a
-                  href='https://github.com/thinkful-ei18/alisha_brian_spacedRep_server'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={require('../images/github_icon.png')} alt='link to server repo' className='github-icon' />
-                </a>
-              </div>
-              <div>
-                <a href='https://github.com/thinkful-ei18/Alisha_Brian_SpacedRep_Client'
-                  className='github-repo-link'> 
-                  Client 
-                </a>
-                <a href='https://github.com/thinkful-ei18/alisha_brian_spacedRep_server'
-                  className='github-repo-link'> 
-                  Server 
-                </a>
-              </div>
-            </div>
           </div>
 
           
@@ -147,46 +56,48 @@ export default class Portfolio extends Component {
               For The Win 
             </a>
             <p className='portfolio-services'>FULL STACK DEVELOPMENT • UI/UX DESIGN</p>
-            <p className='portfolio-project-tagline'>
-              The basketball enthusiast's preferred fantasy basketball league and stat tracker.
+          </div>
+
+        </div>
+
+        <div className='row-two' >
+
+          <div className='project-four'>
+            <img src={require('../images/portfolio/69th_mockup.png')} 
+              alt='69th CoC Mockup' 
+              className='portfolio-responsive-mock' />
+            <p className='portfolio-project-title'> 
+              69th Street 
             </p>
-            <p className='live-porfolio-par'>
-              <a
-                href='https://for-the-win-app.netlify.com/'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='live-portfolio-link'
-              >View the Live Site
-              </a>
+            <p className='portfolio-services'> 
+              VISUAL BRAND IDENTITY • PRINT DESIGN • PHOTOGRAPHY
             </p>
-            <div className='github-repos'>
-              <div>
-                <a
-                  href='https://github.com/thinkful-ei18/for_the_win_client'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={require('../images/github_icon.png')} alt='link to client repo' className='github-icon' />
-                </a>
-                <a
-                  href='https://github.com/thinkful-ei18/for_the_win_server'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={require('../images/github_icon.png')} alt='link to server repo' className='github-icon' />
-                </a>
-              </div>
-              <div>
-                <a href='https://github.com/thinkful-ei18/for_the_win_client'
-                  className='github-repo-link'> 
-                  Client 
-                </a>
-                <a href='https://github.com/thinkful-ei18/for_the_win_server'
-                  className='github-repo-link'> 
-                  Server 
-                </a>
-              </div>
-            </div>
+          </div>
+
+
+          <div className='project-five'>
+            <img src={require('../images/portfolio/ncss_mockup.png')} 
+              alt='NCSS Mockup' 
+              className='portfolio-responsive-mock' />
+            <p className='portfolio-project-title'> 
+              NCSS 
+            </p>
+            <p className='portfolio-services'> 
+              VISUAL BRAND IDENTITY • PRINT DESIGN 
+            </p>
+          </div>
+
+          
+          <div className='project-six'>
+            <img src={require('../images/portfolio/frsh_mkt_mockup.png')} 
+              alt='Fresh Market Mockup' 
+              className='portfolio-responsive-mock' />
+            <p className='portfolio-project-title'> 
+              Fresh Market 
+            </p>
+            <p className='portfolio-services'> 
+              VISUAL BRAND IDENTITY • PRINT DESIGN • PHOTOGRAPHY
+            </p>
           </div>
 
         </div>
