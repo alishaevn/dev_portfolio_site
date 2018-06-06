682544718cacc5347f590a3f42cad995d401064f
alishaevn <alieva.inc@gmail.com>
2018-06-06 10:50:53 -0700

    added all design routes

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import Main from './components/Main'
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
-import About from './components/AboutMe';
-import Contact from './components/Contact';
 import Safer from './components/portfolio/Safer';
 import FoodiePhonetics from './components/portfolio/FoodiePhonetics';
 import ForTheWin from './components/portfolio/ForTheWin';
+import coc69th from './components/portfolio/69th';
 import NCSS from './components/portfolio/NCSS';
+import FreshMarket from './components/portfolio/FreshMarket';
+import About from './components/AboutMe';
+import Contact from './components/Contact';
 
 
 export default class Routes extends Component {
@@ -26,7 +28,9 @@ export default class Routes extends Component {
             <Route exact path='/safer' component={Safer} />
             <Route exact path='/foodiephonetics' component={FoodiePhonetics} />
             <Route exact path='/forthewin' component={ForTheWin} />
+            <Route exact path='/69th' component={coc69th} />
             <Route exact path='/ncss' component={NCSS} />
+            <Route exact path='/freshmarket' component={FreshMarket} />
             <About />
             <Contact />
 
