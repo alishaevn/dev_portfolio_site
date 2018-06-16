f529e99c8dc2264c17cb0ee1f60ae45849b70d9d
alishaevn <alieva.inc@gmail.com>
2018-06-16 00:14:11 -0700

    removed commented out code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import { HashLink } from 'react-router-hash-link'
-import smoothscroll from 'smoothscroll-polyfill'
+import React, { Component } from 'react';
+import { HashLink } from 'react-router-hash-link';
+import smoothscroll from 'smoothscroll-polyfill';
 import { SocialIcon } from 'react-social-icons';
 
 import './home.css'
@@ -42,9 +42,6 @@ export default class Home extends Component {
       marginLeft: 5
     }
 
-    // let style;
-    // this.state.showMenu ? style = 'menu-button-clicked' : style = 'menu-button'
-    
     return (
       <div className='home-component'>
         <nav>
@@ -97,12 +94,6 @@ export default class Home extends Component {
                 className='menu-hashlink'>
                 ABOUT
               </HashLink>
-              {/* <HashLink 
-                to="/#contact"
-                smooth={true}
-                className='menu-hashlink'>
-                CONTACT
-              </HashLink> */}
               <a className='menu-hashlink'
                 href='https://www.linkedin.com/in/alishaevans/'
                 alt='LinkedIn'
@@ -130,14 +121,6 @@ export default class Home extends Component {
         <div className='title'>
           <p className='job-description'> full stack developer, brand identity designer & photographer</p>
         </div>
-        {/* <button className='to-portfolio-btn'>
-          <HashLink 
-            to='/#portfolio' 
-            smooth={true}
-            className='to-portfolio-hashlink'>
-          portfolio
-          </HashLink>
-        </button> */}
         <div className='home-social-media-icons'>
           <SocialIcon url='https://www.linkedin.com/in/alishaevans/' style={socialIconStyles} color='#5E548E' />
           <SocialIcon url='https://github.com/alishaantoinette' style={socialIconStyles} color='#5E548E' />
@@ -154,4 +137,5 @@ export default class Home extends Component {
   - https://github.com/rafrex/react-router-hash-link
   - https://unicode-table.com/en/#2261
   - https://www.w3schools.com/charsets/ref_utf_misc_symbols.asp
+  - http://jaketrent.github.io/react-social-icons/
  */
