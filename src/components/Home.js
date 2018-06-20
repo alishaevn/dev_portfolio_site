94a9c9f3c3aab40b6044f06a8fa433d408bb074c
alishaevn <alieva.inc@gmail.com>
2018-06-20 12:22:04 -0700

    removed navbar from home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react';
-import { HashLink } from 'react-router-hash-link';
-import smoothscroll from 'smoothscroll-polyfill';
 import { SocialIcon } from 'react-social-icons';
+
+import NavBar from './NavBar';
 
 import './home.css'
 
-smoothscroll.polyfill();
 
 export default class Home extends Component {
-  constructor() {
-		super();
-
-		this.state = {
-			showMenu: false,
-		};
-
-		this.showMenu = this.showMenu.bind(this);
-		this.closeMenu = this.closeMenu.bind(this);
-	}
-
-	showMenu(event) {
-		event.preventDefault();
-
-		this.setState({ showMenu: true }, () => {
-			document.addEventListener('click', this.closeMenu);
-		});
-	}
-
-	closeMenu() {
-		this.setState({ showMenu: false }, () => {
-			document.removeEventListener('click', this.closeMenu);
-		});
-	}
   
   render() {
 
@@ -44,86 +19,7 @@ export default class Home extends Component {
 
     return (
       <div className='home-component'>
-        <nav>
-          { this.state.showMenu
-            ? (
-              <button className='menu-button-clicked' onClick={this.showMenu}>
-            X
-          </button>
-            )
-            : (
-              <button className='menu-button' onClick={this.showMenu}>
-            &#9776;
-          </button>
-            )
-          }
-          { this.state.showMenu 
-            ? (
-						<div className="dropdown-menu">
-							<HashLink 
-                to="/#portfolio"
-                smooth={true}
-                className='menu-hashlink'> 
-                PORTFOLIO 
-              </HashLink>
-							<HashLink 
-                to="/#about"
-                smooth={true}
-                className='menu-hashlink'>
-                ABOUT
-              </HashLink>
-              <a className='menu-hashlink'
-                href='https://www.linkedin.com/in/alishaevans/'
-                alt='LinkedIn'
-                target='blank' >
-                LINKEDIN
-              </a>
-              <a className='menu-hashlink'
-                href='https://github.com/alishaantoinette'
-                alt='Github'
-                target='blank' >
-                GITHUB
-              </a>
-              <a className='menu-hashlink'
-                href='mailto:alieva.inc@gmail.com'
-                alt='Email' >
-                EMAIL
-              </a>
-						</div>
-					) 
-          : null }
-          <div className="navigation">
-							<HashLink 
-                to="/#portfolio"
-                smooth={true}
-                className='menu-hashlink'> 
-                PORTFOLIO 
-              </HashLink>
-							<HashLink 
-                to="/#about"
-                smooth={true}
-                className='menu-hashlink'>
-                ABOUT
-              </HashLink>
-              <a className='menu-hashlink'
-                href='https://www.linkedin.com/in/alishaevans/'
-                alt='LinkedIn'
-                target='blank' >
-                LINKEDIN
-              </a>
-              <a className='menu-hashlink'
-                href='https://github.com/alishaantoinette'
-                alt='Github'
-                target='blank' >
-                GITHUB
-              </a>
-              <a className='menu-hashlink'
-                href='mailto:alieva.inc@gmail.com'
-                alt='Email' >
-                EMAIL
-              </a>
-						</div>
-        </nav>
+        <NavBar />
 
         <div className='name'>
           <p className='first-name'>alisha</p>
