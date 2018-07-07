6f68b8b4247b26acb96aabcbcf108df2855558f5
alishaevn <alieva.inc@gmail.com>
2018-07-06 23:27:59 -0700

    updated safer path

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// import Main from './components/Main'
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
-import Safer2 from './components/portfolio/Safer2';
+import Safer from './components/portfolio/Safer';
 import FoodiePhonetics from './components/portfolio/FoodiePhonetics';
 import ForTheWin from './components/portfolio/ForTheWin';
 import coc69th from './components/portfolio/69th';
@@ -23,21 +22,17 @@ export default class Routes extends Component {
       <Router>
         <div className='appPage'>
           <main className='main' role='main'>
-            {/* <Home /> */}
             <Route exact path='/' component={Home} />
             <Route exact path='/' component={Portfolio} />
             <Route exact path='/' component={About} />
             <Route exact path='/' component={Footer} />
             
-            <Route exact path='/safer' component={Safer2} />
+            <Route exact path='/safer' component={Safer} />
             <Route exact path='/foodiephonetics' component={FoodiePhonetics} />
             <Route exact path='/forthewin' component={ForTheWin} />
             <Route exact path='/69th' component={coc69th} />
             <Route exact path='/ncss' component={NCSS} />
             <Route exact path='/freshmarket' component={FreshMarket} />
-            {/* <About /> */}
-            {/* <Contact /> */}
-
           </main>
         </div>
       </Router>
