cdc6d3d53e2c9db854b709c722edb815a4f2a9f5
alishaevn <alieva.inc@gmail.com>
2018-07-06 23:27:20 -0700

    new layout

--- src/components/portfolio/Safer.js
+++ src/components/portfolio/Safer.js
@@ -1,50 +1,73 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+
+import NavBar from '../NavBar';
+import Footer from '../Footer';
 
 import './projects.css';
 
 
 export default class Safer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      close: false
-    }
+
+  componentDidMount() {
+    window.scrollTo(0, 0)
   }
 
   render() {
-    if (this.state.close) {
-      return <Redirect to='/' />
-    } 
 
-    return (
-      <div className='project-component'>
-        <p className='close-portfolio'
-          onClick={() => this.setState({ close: true})} > 
-            X
-        </p>
-
-          <div className='portfolio-project'>
-            <a className='project-title'
-              href='https://safer.netlify.com/' 
+    return(
+      <div>
+        <NavBar />
+        <a className='project-title'
+          href='https://safer.netlify.com/' 
+          target='_blank'
+          rel='noopener noreferrer' > 
+          safeR 
+        </a>
+        <main className='project-container'>
+          <section className='project-intro'>
+            <p className='project-tagline'>
+              SafeReport/safeR aims to keep the community safer by making sure they are up to date on local crimes and incidents in the area.
+              Visitors aren't required to log in to see incidents, they are however required to register if they would like to make a report. It uses geolocation in order to show the most relevant incidents, each of which is indicated by 1 of 5 incident type markers. Creating a report requires a date, time, type and description;  each user can also view all of their reports on their dashboard. 
+            </p>
+            <a className='photo-link'
+              href='https://safer.netlify.com/'
               target='_blank'
-              rel='noopener noreferrer' > 
-              safeR 
+              rel='noopener noreferrer' >
+              <img className='project-responsive-mock'
+                src={require('../../images/portfolio/safer_responsive.png')} 
+                alt='safeR Mockup' />
             </a>
-            <div className='opening-sequence'>
-              <a className='photo-link'
-                href='https://safer.netlify.com/'
-                target='_blank'
-                rel='noopener noreferrer' >
-                <img className='project-responsive-mock'
-                  src={require('../../images/portfolio/safer_responsive.png')} 
-                  alt='safeR Mockup' />
-              </a>
-              <p className='project-summary'>
-              SafeReport/safeR is an app that aims to keep the community safer by making sure they are up to date on local crimes or incidents in the area. When the app loads the user is greeted with a map with reports pinned in their local area. If the user chooses to sign up and login, they can submit a report or view the dashboard for previous reports they submitted themselves.
+          </section>
+          <p className='project-services'> FULL STACK DEVELOPMENT • UI/UX DESIGN • REACT NATIVE </p>
+          
+          <section className='project-details'>
+            <div className='info-box'>
+              <h2 className='project-heading'> The Motivation </h2>
+              <p className='project-info'>
+                The initial idea for the app was to allow users to report hit and run's with details that law enforcement would be able to use to investigate cases. It quickly evolved into a larger idea that allows users to report various types of incidents so that local residents as well as local law enforcement may be aware.
               </p>
             </div>
-            <p className='project-services'> FULL STACK DEVELOPMENT • UI/UX DESIGN • REACT NATIVE </p>
+            <div className='info-box'>
+              <h2 className='project-heading'> My Role </h2>
+              <p className='project-info'>
+                I worked on this app with 4 other developers who affectionately called ourselves MAAJAC. My participation was on the front end of the web and mobile apps creating the custom incident markers, working on the map, the dashboard and design.
+              </p>
+            </div>
+          </section>
+          <div className='photo-bank'>
+              <img className='project-photos-row'
+                src={require('../../images/portfolio/safeR/safer1.png')} 
+                alt='NCSS Mockup' />
+              <img className='project-photos-row'
+                src={require('../../images/portfolio/safeR/safer2.png')} 
+                alt='NCSS Mockup' />
+              <img className='project-photos-row'
+                src={require('../../images/portfolio/safeR/safer3.png')} 
+                alt='NCSS Mockup' />
+              <img className='project-photos-row'
+                src={require('../../images/portfolio/safeR/safer_native.jpg')} 
+                alt='NCSS Mockup' />
+            </div>
             <p className='live-portfolio-par'>
               <a className='live-portfolio-link'
                 href='https://safer.netlify.com/'
@@ -52,50 +75,55 @@ export default class Safer extends Component {
                 rel='noopener noreferrer' >
                   View the Live Site
               </a>
-            </p>
-            <div className='github-repos'>
-              <div>
-                <a href='https://github.com/thinkful-ei18/maajac-client'
-                  target='_blank'
-                  rel='noopener noreferrer' >
-                    <img className='github-icon'
-                      src={require('../../images/github_icon.png')} 
-                      alt='link to client repo' />
-                </a>
-                <a href='https://github.com/thinkful-ei18/majaac-server'
-                  target='_blank'
-                  rel='noopener noreferrer' >
-                    <img className='github-icon'
-                      src={require('../../images/github_icon.png')} 
-                      alt='link to server repo' />
-                </a>
-                <a href='https://github.com/thinkful-ei18/MOBILE-maajac-client'
-                  target='_blank'
-                  rel='noopener noreferrer' >
-                    <img className='github-icon'
-                      src={require('../../images/github_icon.png')} 
-                      alt='link to mobile repo' />
-                </a>
-              </div>
-              <div>
-                <a className='github-repo-link'
-                  href='https://github.com/thinkful-ei18/maajac-client' > 
-                    Client 
-                </a>
-                <a className='github-repo-link'
-                  href='https://github.com/thinkful-ei18/majaac-server' > 
-                    Server 
-                </a>
-                <a className='github-repo-link'
-                  href='https://github.com/thinkful-ei18/MOBILE-maajac-client' > 
-                    Mobile 
-                </a>
-              </div>
+          </p>
+          <div className='github-repos'>
+            <div>
+              <a href='https://github.com/thinkful-ei18/maajac-client'
+                target='_blank'
+                rel='noopener noreferrer' >
+                  <img className='github-icon'
+                    src={require('../../images/github_icon.png')} 
+                    alt='link to client repo' />
+              </a>
+              <a href='https://github.com/thinkful-ei18/majaac-server'
+                target='_blank'
+                rel='noopener noreferrer' >
+                  <img className='github-icon'
+                    src={require('../../images/github_icon.png')} 
+                    alt='link to server repo' />
+              </a>
+              <a href='https://github.com/thinkful-ei18/MOBILE-maajac-client'
+                target='_blank'
+                rel='noopener noreferrer' >
+                  <img className='github-icon'
+                    src={require('../../images/github_icon.png')} 
+                    alt='link to mobile repo' />
+              </a>
+            </div>
+            <div>
+              <a className='github-repo-link'
+                href='https://github.com/thinkful-ei18/maajac-client' > 
+                  Client 
+              </a>
+              <a className='github-repo-link'
+                href='https://github.com/thinkful-ei18/majaac-server' > 
+                  Server 
+              </a>
+              <a className='github-repo-link'
+                href='https://github.com/thinkful-ei18/MOBILE-maajac-client' > 
+                  Mobile 
+              </a>
             </div>
           </div>
-
+        </main>
+        <Footer />
       </div>
     )
-
   }
 }
+
+
+/*
+Resources:
+ - https://reacttraining.com/react-router/web/guides/scroll-restoration 
+ */
