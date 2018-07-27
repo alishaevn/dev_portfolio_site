6d385702173090e4b7829731584a9e404fdcd5f7
alishaevn <alieva.inc@gmail.com>
2018-07-26 20:13:39 -0700

    V2 init commit

---
+++ src/components/design/FreshMarket.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+export default class FreshMarket extends Component {
+
+  // componentDidMount() {
+  //   window.scrollTo(0, 0)
+  // }
+
+  render() {
+
+    return (
+      <article className='fresh-market'>
+        
+      </article>
+    )
+
+  }
+}
---
+++ src/components/design/NCSS.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class NCSS extends Component {
+
+  // componentDidMount() {
+  //   window.scrollTo(0, 0)
+  // }
+
+  render() {
+
+    return (
+      <article className='ncss'>
+        
+      </article>
+    )
+
+  }
+}
