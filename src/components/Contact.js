de55f13736c82951fe7b22783572945adb1d01a2
alishaevn <alieva.inc@gmail.com>
2018-07-27 15:49:17 -0700

    design complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { Link } from 'react-router-dom';
+
+import '../styles/contact.css';
+
 
 export default class Contact extends Component {
   render() {
 
     const socialIconStyles = {
-      height: 35,
-      width: 35,
-      marginRight: 5,
-      marginLeft: 5
+      height: 40,
+      width: 40,
+      marginRight: 8,
+      marginLeft: 8,
+      marginTop: 25,
+      marginBottom: 25
     }
 
     return (
       <footer className='contact' id='contact'>
+        <h3 className=''>
+          I’d love to hear from you about employment, networking or mentorship opportunities! </h3>
+        <p className='resume'>
+          Feel free to view my <Link to='resume' className='link contact-link'>resume</Link>.</p>
+        <p className='contact-info'>
+          Send me an <a href='mailto:alieva.inc@gmail.com' className='link contact-link'>email</a>, 
+          chat with me on <a href='https://twitter.com/thatgirlt0nii' className='link contact-link'>Twitter</a>,
+          connect with me on <a href='https://www.linkedin.com/in/alishaevans/' className='link contact-link'>LinkedIn </a> 
+          or view my <a href='https://github.com/alishaantoinette' className='link contact-link'>Github</a>.
+        </p>
 
-        <div className='footer-social-media-icons'>
-          <SocialIcon url='https://www.linkedin.com/in/alishaevans/' style={socialIconStyles} color='#5E548E' />
-          <SocialIcon url='https://github.com/alishaantoinette' style={socialIconStyles} color='#5E548E' />
-          <SocialIcon url='mailto:alieva.inc@gmail.com' style={socialIconStyles} color='#5E548E' />
+        <div className='social-media-icons'>
+          <SocialIcon url='mailto:alieva.inc@gmail.com' style={socialIconStyles} color='#2C8199' />
+          <SocialIcon url='https://twitter.com/thatgirlt0nii' style={socialIconStyles} color='#2C8199' />
+          <SocialIcon url='https://www.linkedin.com/in/alishaevans/' style={socialIconStyles} color='#2C8199' />
+          <SocialIcon url='https://github.com/alishaantoinette' style={socialIconStyles} color='#2C8199' />
         </div>
 
-
-          <p className='copyright-info'>© Copyright Alisha A. Evans 2018.</p>
+        <h3 className='copyright-info'>
+          © Alisha A. Evans 2018.</h3>
       </footer>
     )
-
   }
 }
+
+/*
+  Resources:
+  - http://jaketrent.github.io/react-social-icons/
+ */
