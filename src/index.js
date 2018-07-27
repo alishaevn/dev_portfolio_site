58ab3d8a6df94d8a26738f43755b8c7e42157b9f
alishaevn <alieva.inc@gmail.com>
2018-07-27 13:18:45 -0700

    formatting change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
 
+import './index.css';
+
+
 ReactDOM.render(
   <Routes />, 
-  document.getElementById('root')
-);
+  document.getElementById('root') );
 registerServiceWorker();
