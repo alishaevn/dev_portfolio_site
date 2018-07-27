df43df0d719102483fbf77f909accbb6c26413fb
alishaevn <alieva.inc@gmail.com>
2018-07-26 22:53:06 -0700

    page completed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 
+import Navigation from './Navigation';
+
+import '../styles/intro.css';
+
 export default class Intro extends Component {
   
   render() {
 
+    let size = window.innerWidth;
+    let query;
+
+    if (size< 576) {
+      query='extra-small';
+    }
+    else if (size< 576) {
+      query='small';
+    }
+    else if (size< 576) {
+      query='medium';
+    }
+    else if (size< 576) {
+      query='large';
+    }
+    else if (size< 576) {
+      query='extra-large';
+    }
+
     return (
-      <section className='intro' id='intro'>
-        
+      <section className={query} id='intro'>
+        <Navigation />
+
+        <p className='intro-tagline'>Detail oriented, problem solving, creative thinking</p>
+        <h1 className='intro-title'>Front-End Developer</h1>
       </section>
     )
 
   }
 }
-
-/*
-  Resources:
-  - https://github.com/rafrex/react-router-hash-link
-  - http://jaketrent.github.io/react-social-icons/
- */
