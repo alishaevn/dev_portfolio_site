c5e80b0e053413a88b544947d15e2fffd63747b1
alishaevn <alieva.inc@gmail.com>
2018-07-27 23:08:10 -0700

    layout complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
+import Navigation from './Navigation';
+
+import '../styles/header.css';
+
+
 export default class Header extends Component {
 
   render() {
 
     return(
       <section className='header'>
-        
+        <Navigation />
+        <h1 className='header-title'>
+          {this.props.title} </h1>
+        <p className='header-tagline'>
+          {this.props.tagline} </p>
       </section>
     )
   }
