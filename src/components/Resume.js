147051c62ea316df7cd87316f64868a2f2aef1e7
alishaevn <alieva.inc@gmail.com>
2018-07-28 15:30:40 -0700

    base outline

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
+import Header from './Header';
+import Contact from './Contact';
+
+import '../styles/caseStudies.css';
+
 export default class Resume extends Component {
 
   render() {
+    let title = this.props.match.path === '/resume'
+      ? 'RESUME'
+      : ''
 
     return(
       <section className='resume'>
+        <Header 
+          title={title} />
+        
+        <Contact />
         
       </section>
     )
