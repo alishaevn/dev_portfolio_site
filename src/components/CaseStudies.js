e2b40bbe163036cec5730ee678f0a3081cefbf49
alishaevn <alieva.inc@gmail.com>
2018-07-29 22:48:35 -0700

    new classes for XL query

--- src/components/CaseStudies.js
+++ src/components/CaseStudies.js
@@ -11,31 +11,34 @@ export default class CaseStudies extends Component {
       <section className='case-studies' id='case-studies'>
         <h1 className='cs-title'> 
           Case Studies </h1>
-        <Link to='/safer'
-          className='link'>
-          <img className='cs-mockup'
-            src={require('../images/studies/safer_mockup.png')} 
-            alt='Safer Mockup'/>
-          <p className='cs-mockup-title'>
-            SAFER </p> 
+        <section className='cs-2x2'>
+          <Link to='/safer'
+            className='link'>
+            <img className='cs-mockup project-one'
+              src={require('../images/studies/safer_mockup.png')} 
+              alt='Safer Mockup'/>
+            <p className='cs-mockup-title'>
+              SAFER </p> 
             </Link>
-        <Link to='/foodiephonetics'
-          className='link'>
-          <img className='cs-mockup'
-            src={require('../images/studies/fp_mockup.png')} 
-            alt='Foodie Phonetics Mockup'/>
-          <p className='cs-mockup-title'>
-            FOODIE PHONETICS </p>
+        </section>
+        <section className='cs-2x2'>
+          <Link to='/foodiephonetics'
+            className='link'>
+            <img className='cs-mockup project-two'
+              src={require('../images/studies/fp_mockup.png')} 
+              alt='Foodie Phonetics Mockup'/>
+            <p className='cs-mockup-title title-two'>
+              FOODIE PHONETICS </p>
             </Link>
-        <Link to='/forthewin'
-          className='link'>
-          <img className='cs-mockup'
-            src={require('../images/studies/ftw_mockup.png')} 
-            alt='For The Win Mockup'/>
-          <p className='cs-mockup-title'>
-            FOR THE WIN </p>
+          <Link to='/forthewin'
+            className='link'>
+            <img className='cs-mockup project-three'
+              src={require('../images/studies/ftw_mockup.png')} 
+              alt='For The Win Mockup'/>
+            <p className='cs-mockup-title'>
+              FOR THE WIN </p>
             </Link>
-        
+          </section>
       </section>
     )
 
