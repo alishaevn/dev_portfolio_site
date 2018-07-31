95fb6632199457656dfeee34f772b680a9af66ab
alishaevn <alieva.inc@gmail.com>
2018-07-31 12:52:46 -0700

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -17,12 +17,14 @@ export default class About extends Component {
         <h2 className='about-welcome'>
           My name is Alisha, thanks for stopping by.</h2>
         <p className='about-me'> 
-          As someone educated in Graphic Design & Full Stack Development, I can "speak the language" of Web Designers & Back End Developers; thus bridging the gap across the web development process. My personal & professional experiences with leadership, education & communication would prove valuable on a team of fellow motivated creatives. Furthermore, I’m continually striving to be better so I welcome feedback & respond accordingly.</p>
-
-        <p className='about-me'> 
+          As someone educated in Graphic Design & Full Stack Development, I understand the jargon & methodologies of Web Designers & Back End Developers; thus bridging the gap across the web development process. My inquisitiveness, integrity, creativity & overall friendly demeanor have led me to have positive interactions with my peers, co-workers, teachers, managers & more. Furthermore, my personal & professional experiences with leadership, education & communication would prove valuable on a team of fellow motivated professionals. 
+          <br /><br />
+          The process of actualizing an idea into a working product that people can interact with & be benefited by is very rewarding for me. As such, I’m continually striving to be better so I welcome feedback & respond accordingly. I'm also a quick learner & collaborative worker who’s dedicated to mutual growth.
+          <br /><br />
           Apart from development & design I enjoy photography
             (<Link to='/photos' className='link about-link'>see here</Link>)
-          , brain games, coaching volleyball & being a basketball fan. </p>
+          , brain games, coaching volleyball & being a basketball fan.
+          </p>
 
         <h2 className='heading'>
           Applicable skills:</h2>
@@ -36,7 +38,7 @@ export default class About extends Component {
               <h5 className='skill-type'>
                 Front End Development</h5>
               <p className='skill-description'>
-                My love for the aesthetics & challenges presented in Front End Development drive me to code efficient & beautiful websites using semantic HTML, CSS, Javascript and front end technologies such as React & Redux as necessary.</p>
+                My love for the aesthetics & challenges presented in Front End Development drive me to code efficient & beautiful websites. The technologies I work with include, but are not limited to: HTML5, CSS3, SASS, JavaScript, React, Redux, jQuery, Git, Adobe CS, Sketch, MongoDB, Node & Express.</p>
             </div>
           </aside>
           <aside className='skillset'>
@@ -48,7 +50,7 @@ export default class About extends Component {
               <h5 className='skill-type'>
                 User Interface Design</h5>
               <p className='skill-description'>
-                Details matter. I’m experienced with designing wireframes, performing market research & creating user personas in order to provide engaging, yet simplistic client interactions.</p>
+                Details matter. I’m experienced with designing wireframes, storyboards, performing market research & creating user personas in order to provide engaging, yet simplistic client interactions.</p>
             </div>
           </aside>
           <aside className='skillset'>
