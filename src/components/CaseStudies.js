1cce65dd6d00abddff91e7ff69220f594fe657bd
alishaevn <alieva.inc@gmail.com>
2018-07-31 15:22:19 -0700

    edits for L media query

--- src/components/CaseStudies.js
+++ src/components/CaseStudies.js
@@ -11,28 +11,26 @@ export default class CaseStudies extends Component {
       <section className='case-studies' id='case-studies'>
         <h1 className='cs-title'> 
           Case Studies </h1>
-        <section className='cs-2x2'>
+        <section className='cs-3x1'>
           <Link to='/safer'
-            className='link'>
-            <img className='cs-mockup project-one'
+            className='link project-one'>
+            <img className='cs-mockup'
               src={require('../images/studies/safer_mockup.png')} 
               alt='Safer Mockup'/>
             <p className='cs-mockup-title'>
               SAFER </p> 
             </Link>
-        </section>
-        <section className='cs-2x2'>
           <Link to='/foodiephonetics'
-            className='link'>
-            <img className='cs-mockup project-two'
+            className='link project-two'>
+            <img className='cs-mockup'
               src={require('../images/studies/fp_mockup.png')} 
               alt='Foodie Phonetics Mockup'/>
             <p className='cs-mockup-title title-two'>
               FOODIE PHONETICS </p>
             </Link>
           <Link to='/forthewin'
-            className='link'>
-            <img className='cs-mockup project-three'
+            className='link project-three'>
+            <img className='cs-mockup'
               src={require('../images/studies/ftw_mockup.png')} 
               alt='For The Win Mockup'/>
             <p className='cs-mockup-title'>
