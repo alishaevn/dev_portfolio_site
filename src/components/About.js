3c5a3f60561bf9543584127ef64a80d0bf3ba36d
alishaevn <alieva.inc@gmail.com>
2018-09-05 13:44:24 -0700

    re-worded

--- src/components/About.js
+++ src/components/About.js
@@ -17,13 +17,11 @@ export default class About extends Component {
         <h2 className='about-welcome'>
           My name is Alisha, thanks for stopping by.</h2>
         <p className='about-me'> 
-          As someone educated in Graphic Design and Full Stack Development, I understand the jargon and methodologies of Web Designers and Web Developers; thus bridging the gap across the web development process. My inquisitiveness, integrity, creativity and overall friendly demeanor have led me to have positive interactions with my peers, co-workers, teachers, managers and more. Furthermore, my personal and professional experiences with leadership, education and communication would prove valuable on a team of fellow motivated professionals. 
-          <br /><br />
-          I'm a quick learner and collaborative worker who’s dedicated to mutual growth. The process of actualizing an idea into a working product that people can interact with and be benefited by is also very rewarding for me. As such, I’m continually striving to be better so I welcome feedback and respond accordingly. 
+          Educated in Graphic Design and Web Development, I understand the jargon and methodologies of both disciplines; thus bridging the gap between them. I enjoy creating positive user experiences through beautiful user interfaces from scratch, or with Content Management Systems. As such, encouraging and following through on feedback about my work is one way that I continue to better my skillset. My personal and professional experiences with leadership, education and communication would prove valuable on a team of fellow motivated professionals. Furthermore, my inquisitiveness, integrity, creativity and overall friendly demeanor have led me to have positive interactions with my peers, co-workers, teachers, managers and more.              
           <br /><br />
           Apart from development & design I enjoy photography
             (<Link to='/photos' className='link about-link'>see here</Link>)
-          , brain games, coaching volleyball & being a basketball fan.
+          , brain games, coaching volleyball & being a sports (especially basketball) fan.
           </p>
 
         <h2 className='heading'>
@@ -38,7 +36,7 @@ export default class About extends Component {
               <h5 className='skill-type'>
                 Web Development</h5>
               <p className='skill-description'>
-                I have a love for the aesthetics and challenges presented in Front End Development as well as an appreciation for the logic required in Back End Development. The technologies I work with include, but are not limited to: HTML5, CSS3, SASS, JavaScript, React, Redux, jQuery, Git, Adobe CS, Sketch, MongoDB, Node & Express.</p>
+                The technologies I work with include, but are not limited to: HTML5, CSS3, SASS, JavaScript, React, Redux, jQuery, Git, GitHub, Adobe Creative Suite, Sketch, MongoDB, Node & Express.</p>
             </div>
           </aside>
           <aside className='skillset'>
