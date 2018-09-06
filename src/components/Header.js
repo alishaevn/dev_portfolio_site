14669672e468e4674dd61188551fb0e0ca632261
alishaevn <alieva.inc@gmail.com>
2018-09-06 00:25:38 -0700

    added title class condidtion

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,12 @@ import '../styles/css/header.css';
 export default class Header extends Component {
 
   render() {
-    let size = this.props.size ? this.props.size : 'header'
+    let title = this.props.title ? 'header-title' : 'no-header-title'
     
     return(
-      <section className={size}>
+      <section className='header'>
         <Navigation />
-        <h1 className='header-title'>
+        <h1 className={title}>
           {this.props.title} </h1>
         <p className='header-tagline'>
           {this.props.tagline} </p>
