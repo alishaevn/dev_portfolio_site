661c666d9b09d24fab643dc9e6d8a6d9daed0bdd
alishaevn <alieva.inc@gmail.com>
2018-09-05 23:28:14 -0700

    added a second class to links

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -32,8 +32,8 @@ export default class Resume extends Component {
                 <p className='resume-text'> 
                   619-535-8245 <br />
                   alieva.inc@gmail.com <br />
-                  <a href='https://github.com/alishaantoinette' className='link'>Github</a> <br />
-                  <a href='https://www.linkedin.com/in/alishaevans/' className='link'>LinkedIn </a>
+                  <a href='https://github.com/alishaantoinette' className='link resume-link'>Github</a> <br />
+                  <a href='https://www.linkedin.com/in/alishaevans/' className='link resume-link'>LinkedIn </a>
                 </p>
                 <h3 className='resume-header-blue'>WHY ME?</h3>
                 <p className='resume-text'>I’m a proven quick learner with several years experience in problem solving. A leader, effective communicator and team player on the court and in the office.  I’m steadily working on improving my skillset and am eager to make positive contributions as a web developer.</p>
